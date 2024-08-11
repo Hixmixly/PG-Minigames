a86bb10cd16dbdffb49c0b1d26f332ec455531c0
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-08-11 13:04:01 -0700

    fixed spelling mistakesand changed deck code

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Deck from '../src/components/deck';
 
 function App() {
-    const deck = new Deck();
-    console.log(deck.cards);
+    const[cards, setCards] = useState([]);
+    
+    useEffect(() => {
+        const deck = new Deck();
+        
+        console.log(deck.cards);
+        
+        deck.shuffle();
+        
+        setCards(deck.draw(5)) 
+    }, []);
+ 
 
     return (
         <div className="App">
             <h1>Go Fish Game</h1>
+            <div className='cards'>
+                {cards.map((card, index) => (
+                    <div key ={index}>
+                        {card.name}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
--- frontend/src/components/deck.js
+++ frontend/src/components/deck.js
@@ -1,42 +1,43 @@
-// Card suites and values 
-const suits = ['C', 'D', 'H', 'S' ]
-const value = ['A', '2','3','4', '5',
-                 '6', '7','8', '9', 
-                 '10', 'J','Q', 'K']
-
-export default class Deck {
-    constructor(cards = newDeck) {
-        this.cards = cards  
+class Deck {
+    constructor(cards = newDeck()) {
+        this.cards = cards;  
     }
 
     get numberOfCards() {
-        return this.cards.length
+        return this.cards.length;
     }
     
 //generates deck randomly 
 shuffle() {
-        for (let i = this.cards.length - 1; i > e; i --){
+        for (let i = this.cards.length - 1; i > 0; i --){
             const newIndex = Math.floor(Math.random() * (i + 1))
             const oldValue = this.cards [newIndex]
             this.cards[newIndex] = this.cards[i]
             this.cards[i] = oldValue
         }
     }
+        draw(count) {
+            return this.cards.splice(0, count); 
+    }
 }
+
 
 class Card {
     constructor(suit, value) {
         this.suit = suit
         this.value = value
     }
+
+    get name(){
+        return `${this.value} of ${this.suit}`;
+    }
 }
 
-function newDeck( ){
-    return suits.flatMap(suit => {
-        return value.map(value => {
-            return new Card(suit, value)
-        })
-    })
+//newDeck function 
+function newDeck() {
+    const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+    const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    return suits.flatMap(suit => values.map(value => new Card(suit, value)));
 }
 
-module.exports = Deck
+export default Deck;
